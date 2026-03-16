1423a537232fbed6517baef6640cc3a772216516
MazenMDev <mazen.m.mahmoud0@gmail.com>
2026-03-16 23:01:36 +0200

    feat(blog-oop): initialize bootstrap and config files, implement App class for dependency management

---
+++ Projects/blog-oop/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'app' => [
+    'name'=> 'My Blog',
+    'debug' => true
+  ],
+  'database' => [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'blog_database',
+    'user' => 'root',
+    'password' => ''
+  ]
+];
---
+++ Projects/blog-oop/core/App.php
@@ -0,0 +1,22 @@
+<?php
+  namespace Core;
+
+  use Exception;
+  use RuntimeException;
+
+  class App {
+    protected static $container = [];
+
+    public static function bind(string $key, mixed $value): void {
+      App::$container[$key] = $value;
+    }
+
+    public static function get(string $key): mixed {
+      if(!array_key_exists($key, App::$container)){
+        throw new Exception("No key found for $key");
+      }
+      return App::$container[$key];
+    }
+  }
+
+?>
